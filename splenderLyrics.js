f9b58e055a14ded1c835288a6bc8e9e3d6bf4049
Vlad Permaz <141409966+VladimirGagarin@users.noreply.github.com>
2025-08-14 18:20:54 +0300

    Updates

---
+++ splenderLyrics.js
@@ -0,0 +1,322 @@
+export const lyrics = [
+  {
+    "start": 99,
+    "end": 10220,
+    "text": {
+      "en": "(dramatic music plays)",
+      "it": ""
+    }
+  },
+  {
+    "start": 10260,
+    "end": 14879,
+    "text": {
+      "en": "When all was hushed and dimmed by sorrow.",
+      "it": ""
+    }
+  },
+  {
+    "start": 15500,
+    "end": 20159,
+    "text": {
+      "en": "And silence wrapped the aching years.",
+      "it": ""
+    }
+  },
+  {
+    "start": 20799,
+    "end": 25399,
+    "text": {
+      "en": "A single breath invoked tomorrow.",
+      "it": ""
+    }
+  },
+  {
+    "start": 26059,
+    "end": 30520,
+    "text": {
+      "en": "And stars broke through our hidden tears.",
+      "it": ""
+    }
+  },
+  {
+    "start": 30899,
+    "end": 34040,
+    "text": {
+      "en": "Gloria splendoris lux",
+      "it": ""
+    }
+  },
+  {
+    "start": 34139,
+    "end": 38479,
+    "text": {
+      "en": "renascentis. Cantemus omnes",
+      "it": ""
+    }
+  },
+  {
+    "start": 38680,
+    "end": 42720,
+    "text": {
+      "en": "in lumine. Gloria",
+      "it": ""
+    }
+  },
+  {
+    "start": 42759,
+    "end": 47239,
+    "text": {
+      "en": "splendoris flamma vivens!. Ecce",
+      "it": ""
+    }
+  },
+  {
+    "start": 47299,
+    "end": 51740,
+    "text": {
+      "en": "novus Novus ordo in floribus!.",
+      "it": ""
+    }
+  },
+  {
+    "start": 53259,
+    "end": 60040,
+    "text": {
+      "en": "(instrumental music plays)",
+      "it": ""
+    }
+  },
+  {
+    "start": 60059,
+    "end": 64659,
+    "text": {
+      "en": "Oh, splendor rise from embers deep.",
+      "it": ""
+    }
+  },
+  {
+    "start": 65619,
+    "end": 69860,
+    "text": {
+      "en": "Like roses blooming after sleep.",
+      "it": ""
+    }
+  },
+  {
+    "start": 70879,
+    "end": 75439,
+    "text": {
+      "en": "You are the song that grief forgets.",
+      "it": ""
+    }
+  },
+  {
+    "start": 76180,
+    "end": 80959,
+    "text": {
+      "en": "The golden flame the night regrets.",
+      "it": ""
+    }
+  },
+  {
+    "start": 81000,
+    "end": 84119,
+    "text": {
+      "en": "Gloria splendoris lux",
+      "it": ""
+    }
+  },
+  {
+    "start": 84199,
+    "end": 88500,
+    "text": {
+      "en": "renascentis. Cantemus omnes",
+      "it": ""
+    }
+  },
+  {
+    "start": 88779,
+    "end": 92839,
+    "text": {
+      "en": "in lumine!. Gloria",
+      "it": ""
+    }
+  },
+  {
+    "start": 92839,
+    "end": 97480,
+    "text": {
+      "en": "splendoris flamma vivens!. Ecce",
+      "it": ""
+    }
+  },
+  {
+    "start": 97860,
+    "end": 101419,
+    "text": {
+      "en": "Novus ordo in floribus!.",
+      "it": ""
+    }
+  },
+  {
+    "start": 102879,
+    "end": 110299,
+    "text": {
+      "en": "(instrumental music plays)",
+      "it": ""
+    }
+  },
+  {
+    "start": 110459,
+    "end": 114619,
+    "text": {
+      "en": "Now is the hour, the moment divine.",
+      "it": ""
+    }
+  },
+  {
+    "start": 115759,
+    "end": 119799,
+    "text": {
+      "en": "Where ashes whisper, \"I still shine.\"",
+      "it": ""
+    }
+  },
+  {
+    "start": 121059,
+    "end": 125319,
+    "text": {
+      "en": "Our hearts reborn, our voices wide.",
+      "it": ""
+    }
+  },
+  {
+    "start": 126299,
+    "end": 130878,
+    "text": {
+      "en": "We bloom like Rome in sacred pride.",
+      "it": ""
+    }
+  },
+  {
+    "start": 131119,
+    "end": 136079,
+    "text": {
+      "en": "Gloria splendoris lux renascentis.",
+      "it": ""
+    }
+  },
+  {
+    "start": 136520,
+    "end": 141139,
+    "text": {
+      "en": "Cantemus omnes  in lumine.",
+      "it": ""
+    }
+  },
+  {
+    "start": 141660,
+    "end": 146639,
+    "text": {
+      "en": "Gloria splendoris flamma vivens!.",
+      "it": ""
+    }
+  },
+  {
+    "start": 147080,
+    "end": 152040,
+    "text": {
+      "en": "Ecce Novus ordo in floribus!.",
+      "it": ""
+    }
+  },
+  {
+    "start": 152080,
+    "end": 160399,
+    "text": {
+      "en": "(instrumental music plays)",
+      "it": ""
+    }
+  },
+  {
+    "start": 160539,
+    "end": 164939,
+    "text": {
+      "en": "Though time may fade our earthly names.",
+      "it": ""
+    }
+  },
+  {
+    "start": 165800,
+    "end": 170059,
+    "text": {
+      "en": "The Splendor stays \u2014 in bloom, in flame..",
+      "it": ""
+    }
+  },
+  {
+    "start": 171119,
+    "end": 175300,
+    "text": {
+      "en": "We walk as petals kissed by grace.",
+      "it": ""
+    }
+  },
+  {
+    "start": 176440,
+    "end": 181219,
+    "text": {
+      "en": "Eternal light upon our face.",
+      "it": ""
+    }
+  },
+  {
+    "start": 181220,
+    "end": 184399,
+    "text": {
+      "en": "Gloria splendoris lux",
+      "it": ""
+    }
+  },
+  {
+    "start": 184459,
+    "end": 188879,
+    "text": {
+      "en": "renascentis. Cantemus omnes",
+      "it": ""
+    }
+  },
+  {
+    "start": 189000,
+    "end": 193039,
+    "text": {
+      "en": "in lumine. Gloria",
+      "it": ""
+    }
+  },
+  {
+    "start": 193099,
+    "end": 197639,
+    "text": {
+      "en": "splendoris flamma vivens!. Ecce",
+      "it": ""
+    }
+  },
+  {
+    "start": 198099,
+    "end": 202079,
+    "text": {
+      "en": "Novus ordo in floribus!.",
+      "it": ""
+    }
+  },
+  {
+    "start": 212819,
+    "end": 224860,
+    "text": {
+      "en": "(instrumental music plays)",
+      "it": ""
+    }
+  }
+];
